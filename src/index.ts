a37299446ac707b678b44fdd9087b35f07cd2643
Mati Kärner <mati.karner@gmail.com>
2021-03-04 03:20:24 +0200

    Allows creating properties from API examples

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ export * from './core';
 // Capabilities
 export * as Capabilities from './capabilities';
 
+// Factories
+export * as Factory from './factories';
+
 // Values
 export * as Values from './values';
